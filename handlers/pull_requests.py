25c7de4ba3b3d642c4ba5824f2f850f29e48a511
Bulat Torgen <bulat.turgunbaev@icloud.com>
2025-06-30 15:48:37 +0600

    initial commit

--- handlers/pull_requests.py
+++ handlers/pull_requests.py
@@ -1,19 +1,29 @@
-
 import os
+import requests
 
 TOKEN = os.getenv("TOKEN")
 HEADERS = {'Authorization': f'Bearer {TOKEN}'}
 
-
 def get_pull_requests(state):
-    """
-    Example of return:
-    [
-        {"title": "Add useful stuff", "num": 56, "link": "https://github.com/boto/boto3/pull/56"},
-        {"title": "Fix something", "num": 57, "link": "https://github.com/boto/boto3/pull/57"},
-    ]
-    """
-
-    # Write your code here
-
-    return []
+    url = "https://api.github.com/repos/boto/boto3/pulls"
+    params = {
+        'state': state,
+        'per_page': 100
+    }
+    
+    response = requests.get(url, headers=HEADERS, params=params)
+    
+    
+    response.raise_for_status()
+    
+    pr_data = response.json()
+    
+    pull_requests = []
+    for pr in pr_data:
+        pull_requests.append({
+            'title': pr['title'],
+            'num': pr['number'],
+            'link': pr['html_url']
+        })
+    
+    return pull_requests
